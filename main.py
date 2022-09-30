a50916177f7a62f6fb5b64100ab51ad5045bbc19
yaros <yaroslavdisiak@gmail.com>
2022-09-30 10:00:19 +0300

    Lab 2 done!

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #dev
+user=int(input("Enter diametrs of circle: "))
+znak= str(input("Whar are you need? P or S: "))
+pi=3.14
+if znak == "P":
+    print(f"P: {int(2*pi * user)} ")
+elif znak == "S":
+    print(f"S: {int(pi*user**2)}")
+else:
+    print("Error! u must enter P or S!")
