82d235f10f2c60c5d2fc609ee16afbd19590e323
yaros <yaroslavdisiak@gmail.com>
2022-09-30 19:53:56 +0300

    HM 3 done

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #dev
+user = int(input("Enter number: "))
+if user == 0:
+    print("Number is equal to zero")
+elif user > 0:
+    print("Number is positive")
+elif user < 0:
+    print("Number is negative")
+else:
+    print("Error")
