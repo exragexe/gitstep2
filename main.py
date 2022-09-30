813619874898aaa26fef9ed5787b00b04cdfda45
yaros <yaroslavdisiak@gmail.com>
2022-09-30 19:50:11 +0300

    HM 2 done

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #dev
+user = int(input("Enter number moon 1-12: "))
+if user ==1:
+    print("January")
+elif user ==2:
+    print("February")
+elif user ==3:
+    print("March")
+elif user ==4:
+    print("April")
+elif user ==5:
+    print("May")
+elif user ==6:
+    print("June")
+elif user ==7:
+    print("July")
+elif user ==8:
+    print("August")
+elif user ==9:
+    print("September")
+elif user ==10:
+    print("October")
+elif user == 11:
+    print("November")
+elif user == 12:
+    print("December")
+else:
+    print("Error")
