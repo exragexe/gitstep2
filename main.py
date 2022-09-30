bea430f56082e1e2a959c23a345143b31f42857c
yaros <yaroslavdisiak@gmail.com>
2022-09-30 19:20:20 +0300

    Lab 5 done!

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #dev
+hours=int(input("Enter hours 0-23: "))
+
+
+if  0 <= hours <= 5:
+    print("Good Night!")
+elif 6 <= hours <=12 :
+    print("Good Morning!")
+elif 13 <=hours <= 16 :
+    print("Good Day!")
+elif 17 <=hours <=23:
+    print("Good Evening!")
+else:
+    print("Error")
