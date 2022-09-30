95fb358e70612e7ca85818e5a18777a69ad95f82
yaros <yaroslavdisiak@gmail.com>
2022-09-30 20:12:29 +0300

    HM 4 done

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #dev
+num1=int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+if num2 > num1:
+    print(num2, num1)
+elif num2 < num1:
+    print(num1, num2)
+elif num1 == num2:
+    print(num1, num2)
+else:
+    print("Error")
