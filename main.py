9e259078641b81bbe60cc21d6e69c191d5e0139c
yaros <yaroslavdisiak@gmail.com>
2022-09-30 19:40:24 +0300

    HM1 done

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #dev
+user = int(input("Enter number 1-7: "))
+if user ==1 :
+    print("Monday")
+elif user ==2:
+    print("Tuesday")
+elif user == 3:
+    print("Wednesday")
+elif user == 4:
+    print("Thursday")
+elif user == 5:
+    print("Friday")
+elif user ==6:
+    print("Saturday")
+elif user == 7:
+    print("Sunday")
+else:
+    print("ERROR")
